6189cad2ddf3bff831b82a3af55341172496048e
Thomas GUIRRIEC <thomas@guirriec.fr>
2024-05-26 05:01:37 +0000

    feat: Alpine Linux 3.20

--- entrypoint_ci.sh
+++ entrypoint_ci.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Wait For OpenStack Swift Available (TCP 8080)
-while ! nc -vz -w 1 swift 8080 >/dev/null 2>&1; do
+while ! swift -A http://swift:8080/auth/v1.0 -U test:tester -K testing stat test >/dev/null 2>&1; do # DevSkim: ignore DS137138
 	sleep 1
 done
 
